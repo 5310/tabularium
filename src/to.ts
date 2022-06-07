6df602c74a48d2f7d31a82a505c17708bfa25f0f
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2022-06-07 11:49:42 +0530

    Add some casters

---
+++ src/to.ts
@@ -0,0 +1,15 @@
+import { yaml } from "../deps/mod.ts"
+
+export const toString = (x: unknown) => String(x)
+
+export const toNumber = (x: unknown) => parseFloat(toString(x).replace(/^[^0-9.]+/, ''))
+
+export const toInteger = (x: unknown) => parseInt(toString(x).replace(/^[^0-9.]+/, ''))
+
+export const toYAML = (x: unknown) => yaml.stringify(x)
+
+export const toJSON = (x: unknown) => JSON.stringify(x)
+
+export const parseYAML = (x: string) => yaml.parse(x)
+
+export const parseJSON = (x: string) => JSON.parse(x)
