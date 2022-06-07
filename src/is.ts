fa7b6f6c3e2aa385902005b525a4161543372254
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2022-06-07 11:18:00 +0530

    Add type validators

---
+++ src/is.ts
@@ -0,0 +1,26 @@
+import { KND, LBL, VAL } from './types.ts'
+
+export const isNull = (x: unknown) => x === null
+
+export const isUndefined = (x: unknown) => x === undefined
+
+export const isBoolean = (x: unknown) => typeof x === 'boolean'
+
+export const isNumber = (x: unknown) => typeof x === 'number' && !isNaN(x)
+
+export const isString = (x: unknown) => typeof x === "string"
+
+export const isArray = (x: unknown) => Array.isArray(x)
+
+export const isObject = (x: unknown) => typeof x === "object" && !isNull(x)
+
+export const isValue = (x: unknown) => isNull(x) || isUndefined(x) || isBoolean(x) || isNumber(x) || isString(x) || isArray(x) || isObject(x)
+
+export const isValueDeep = (x: unknown): boolean => 
+  isArray(x) ? (x as Array<unknown>).every(isValueDeep)
+  : isObject(x) ? Object.values(x as Record<string, unknown>).every(isValueDeep)
+  : isValue(x)
+
+export const isTabula = (x: unknown) => isObject(x) && !isUndefined((x as Record<string, unknown>)[KND])
+
+export const isResult = (x: unknown) => isObject(x) && !isUndefined((x as Record<string, unknown>)[LBL]) && !isUndefined((x as Record<string, unknown>)[VAL])
