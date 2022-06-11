33d1ce2544bbf071ca03053e34a7fd4c5661c13d
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2022-06-11 11:35:36 +0530

    Add random dependency

--- deps/mod.ts
+++ deps/mod.ts
@@ -1,3 +1,4 @@
 export * as lit from './lit.ts'
 export * as yaml from './yaml.ts'
 export * as dice from './dice.ts'
+export * as random from './random.ts'
---
+++ deps/random.ts
@@ -0,0 +1,3 @@
+import { Random } from 'https://cdn.skypack.dev/random@v3.0.6?dts'
+
+export const random = Random
